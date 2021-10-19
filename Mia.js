0733d64112c29419e958b9e914a9981ef38f08a8
jobritomar <89355366+jobritomar@users.noreply.github.com>
2021-10-19 20:18:17 +0100

    Delete Mia.js

--- Mia.js
+++
@@ -1,9 +0,0 @@
-class Mia {
-    constructor(canvas) {
-        this.size=40;
-        this.canvas=canvas;
-        this.ctx=this.canvas.getContext("2d")
-        this.positionX= postionX
-        this.positionY= positionY
-    }
-}
